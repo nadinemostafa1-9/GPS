3c5d88344404feb14cd1dca6ad706ddeab94dc19
SamuelShokry <68712819+SamuelShokry@users.noreply.github.com>
2021-06-09 14:46:53 +0200

    Create Gpt.c

---
+++ Gpt.c
@@ -0,0 +1,43 @@
+ /******************************************************************************
+ *
+ * Module: Gpt
+ *
+ * File Name: Gpt.c
+ *
+ * Description: Source file for TM4C123GH6PM Microcontroller - SysTick Timer Driver.
+ *
+ ******************************************************************************/
+
+#include "Gpt.h"
+#include "tm4c123gh6pm_registers.h"
+
+#define SYSTICK_PRIORITY_MASK  0x1FFFFFFF
+#define SYSTICK_INTERRUPT_PRIORITY  3
+#define SYSTICK_PRIORITY_BITS_POS   29
+
+/************************************************************************************
+* Service Name: SysTick_Start
+* Sync/Async: Synchronous
+* Reentrancy: reentrant
+* Parameters (in): Tick_Time - Time in miliseconds
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: None
+* Description: Function to Setup the SysTick Timer configuration to count n miliseconds:
+*              - Set the Reload value
+*              - Enable SysTick Timer with System clock 16Mhz
+*              - Enable SysTick Timer Interrupt and set its priority
+************************************************************************************/
+void SysTick_Start(uint16 Tick_Time)
+{
+    SYSTICK_CTRL_REG    = 0;                         /* Disable the SysTick Timer by Clear the ENABLE Bit */
+    SYSTICK_RELOAD_REG  = 15999 * Tick_Time;         /* Set the Reload value to count n miliseconds */
+    SYSTICK_CURRENT_REG = 0;                         /* Clear the Current Register value */
+    /* Configure the SysTick Control Register 
+     * Enable the SysTick Timer (ENABLE = 1)
+     * Enable SysTick Interrupt (INTEN = 1)
+     * Choose the clock source to be System Clock (CLK_SRC = 1) */
+    SYSTICK_CTRL_REG   |= 0x07;
+    /* Assign priority level 3 to the SysTick Interrupt */
+    NVIC_SYSTEM_PRI3_REG =  (NVIC_SYSTEM_PRI3_REG & SYSTICK_PRIORITY_MASK) | (SYSTICK_INTERRUPT_PRIORITY << SYSTICK_PRIORITY_BITS_POS);
+}
