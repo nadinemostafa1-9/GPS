e73733a5b3cf4badb8eb6ead17e62024f3c17b5d
Sayed-badr <68440283+Sayed-badr@users.noreply.github.com>
2021-06-09 14:24:48 +0200

    Create buzzer_LED.h

---
+++ buzzer_LED.h
@@ -0,0 +1,20 @@
+ /******************************************************************************
+ *
+ * Module: buzzer
+ *
+ * File Name: buzzer.h
+ *
+ * Description: Header file for buzzer.
+ ******************************************************************************/
+
+#ifndef BUZZER_H_
+#define BUZZER_H_
+
+#include "tm4c123gh6pm_registers.h"
+#include "Platform_Types.h"
+
+void buzzer_init();
+
+void LED_init();
+
+#endif /* BUZZER_H_ */
